c271555c64f22b8060d9e9ef023a26ec07fc678b
Chehak Nayar <chehaknayar@Chehaks-MacBook-Pro.local>
2023-06-16 22:22:35 -0700

    watch and ring model

--- packages/examples/core/primitives/primitives2D.js
+++ packages/examples/core/primitives/primitives2D.js
@@ -1,31 +1,58 @@
 /**
- * 2D Primitives Demonstration
+ * Watch Model
  * @category Creating Shapes
  * @skillLevel 1
- * @description Demonstrating the basics of a variety of 2D primitives
- * @tags circle, square, line, ellipse, polygon, shape, 2d
- * @authors Simon Clark
+ * @description Demonstrating the basics of a variety of 3D primitives
+ * @tags cube, cuboid, sphere, ellipsoid, cylinder, torus, shape, 3d
+ * @authors Rene K. Mueller, Moissette Mark, Simon Clark
  * @licence MIT License
  */
 
-const { arc, circle, ellipse, line, polygon, rectangle, roundedRectangle, square, star } = require('@jscad/modeling').primitives
-const { translate } = require('@jscad/modeling').transforms
+const jscad = require('@jscad/modeling')
+const { cuboid, cylinder } = jscad.primitives
+const { translate, rotateZ } = jscad.transforms
+const { colorize, hexToRgb } = jscad.colors
 
-const main = () => {
-  const allPrimitives = [
-    arc({ center: [-1, -1], radius: 2, startAngle: 0, endAngle: (Math.PI / 2), makeTangent: false, segments: 32 }),
-    line([[1, 1], [-1, -1], [1, -1]]),
-    circle({ radius: 1.8, segments: 10 }),
-    ellipse({ radius: [0.7, 2] }),
-    polygon({ points: [[-3, -1], [3, -1], [3.5, 2], [1.5, 1], [0, 2], [-1.5, 1], [-3.5, 2]] }),
-    rectangle({ size: [2, 3] }),
-    roundedRectangle({ size: [4, 3], roundRadius: 0.7 }),
-    square({ size: 3.5 }),
-    star(),
-    star({ vertices: 9, outerRadius: 2, innerRadius: 0.8, density: 2, startAngle: Math.PI / 18 })
-  ]
-
-  return allPrimitives.map((primitive, index) => translate([(index % 4 - 2) * 6, Math.floor(index / 4 - 2) * 6, 0], primitive))
+const options = {
+  strapColor: [0, 0, 255], // Default strap color is blue
+  dialColor: [0, 100, 100], // Default dial color is white
+  strapWidth: 30, // Default strap width
+  dialRadius: 15 // Default dial radius
 }
 
-module.exports = { main }
+const getParameterDefinitions = () => [
+  { name: 'strapColor', type: 'color', initial: '#0000FF', caption: 'Strap Color' },
+  { name: 'dialColor', type: 'color', initial: '#006464', caption: 'Dial Color' },
+  { name: 'strapWidth', type: 'number', initial: 30, caption: 'Strap Width', min: 1 },
+  { name: 'dialRadius', type: 'number', initial: 15, caption: 'Dial Radius', min: 1 }
+]
+
+const initializeOptions = (params) => {
+  options.strapColor = hexToRgb(params.strapColor)
+  options.dialColor = hexToRgb(params.dialColor)
+  options.strapWidth = params.strapWidth
+  options.dialRadius = params.dialRadius
+}
+
+const main = (params) => {
+  initializeOptions(params)
+
+  const dial = cylinder({ radius: options.dialRadius, height: 4 })
+  const minuteHand = cuboid({ size: [0.5, 10, 5] })
+  const hourHand = cuboid({ size: [10, 0.5, 5] })
+  const strap1 = cuboid({ size: [100, options.strapWidth, 2] })
+  const strap2 = cuboid({ size: [100, options.strapWidth, 2] })
+
+  // Assemble the watch components
+  const watch = [
+    translate([0, 2, 0], colorize(options.dialColor, dial)),
+    translate([2, 3, 0], rotateZ(Math.PI / 6, colorize(options.strapColor, hourHand))),
+    translate([0, -2, 1], rotateZ(Math.PI / 30, colorize(options.strapColor, minuteHand))),
+    translate([20 / 2, 2, 0], colorize(options.strapColor, strap1)),
+    translate([-20 / 2, 2, 0], colorize(options.strapColor, strap2))
+  ]
+
+  return watch
+}
+
+module.exports = { main, getParameterDefinitions }
